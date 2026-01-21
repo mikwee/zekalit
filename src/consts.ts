689bdce664f2efd766ec4862f4d6eef318e9d7f9
mikwee <dev@mikwee.anonaddy.me>
2026-01-22 00:55:14 +0200

    💬 Changed website name and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'זה קליט';
+export const SITE_DESCRIPTION = 'בלוג מוזיקה עצמאי בעברית';
