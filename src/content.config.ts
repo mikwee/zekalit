5bbec9b82081aa2873e7e8f85f2f079b8a67904e
mikwee <dev@mikwee.anonaddy.me>
2026-01-30 23:57:01 +0200

    ✨ Added blog content collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,17 +8,31 @@ const reviews = defineCollection({
 	schema: ({ image }) =>
 		z.object({
 			title: z.string(),
-			artist: z.string().optional(),
-			year: z.number().optional(),
-			rating: z.number().optional(),
+			artist: z.string(),
+			year: z.number(),
+			rating: z.number(),
+			description: z.string().optional(),
+			// Transform string to Date object
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			mbid: z.string().optional(),
+			localCover: image(),
+		}),
+});
+
+const blog = defineCollection({
+	// Load Markdown and MDX files in the `src/content/reviews/` directory.
+	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
 			description: z.string().optional(),
 			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: image().optional(),
-			mbid: z.string().optional(),
-			localCover: image().optional(),
 		}),
 });
 
-export const collections = { reviews };
+export const collections = { reviews, blog };
